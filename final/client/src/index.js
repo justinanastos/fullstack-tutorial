b998239524af2c277559c8034c82b1a772ed487b
Justin Anastos <justin.anastos@gmail.com>
2019-02-04 16:58:49 -0500

    Install non-alpha dependencies and use apollo-link-state

--- final/client/src/index.js
+++ final/client/src/index.js
@@ -6,6 +6,8 @@ import {InMemoryCache} from 'apollo-cache-inmemory';
 import {HttpLink} from 'apollo-link-http';
 import {Query, ApolloProvider} from 'react-apollo';
 import gql from 'graphql-tag';
+import { withClientState } from "apollo-link-state";
+import { ApolloLink } from 'apollo-link';
 
 import Pages from './pages';
 import Login from './pages/login';
@@ -17,16 +19,22 @@ import injectStyles from './styles';
 const cache = new InMemoryCache();
 const client = new ApolloClient({
   cache,
-  link: new HttpLink({
-    uri: 'http://localhost:4000/graphql',
-    headers: {
-      authorization: localStorage.getItem('token'),
-      'client-name': 'Space Explorer [web]',
-      'client-version': '1.0.0',
-    },
-  }),
-  resolvers,
-  typeDefs,
+  // connectToDevTools: true,
+  link: ApolloLink.from([
+    withClientState({ 
+      resolvers,
+      typeDefs,
+      cache,
+    }),
+    new HttpLink({
+      uri: 'http://localhost:4000/graphql',
+      headers: {
+        authorization: localStorage.getItem('token'),
+        'client-name': 'Space Explorer [web]',
+        'client-version': '1.0.0',
+      },
+    }),
+  ]),
 });
 
 cache.writeData({
